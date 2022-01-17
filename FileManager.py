ffc30b2366063601e5e80d4e40abad723922004b
Bartek <bartosz.marmolowski@gmail.com>
2022-01-17 23:26:30 +0100

    'self' added to buttons

--- FileManager.py
+++ FileManager.py
@@ -8,39 +8,39 @@ class FileManager:
     def __init__(self, master):
         self.frame_header = ttk.Frame(master)
         self.frame_header.pack()
+
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
+
         self.frame_footer = ttk.Frame(master)
         self.frame_footer.pack()
 
-        ttk.Label(self.frame_header, text='This script will help you manage files on your computer.').pack()
-        ttk.Label(self.frame_footer, text='by Bartosz Marmołowski').pack(side='bottom')
+        ttk.Label(self.frame_header, text='This script will help you manage files on your computer.')
+        ttk.Label(self.frame_footer, text='aaaaby Bartosz Marmołowski')
 
-        copy_file_button = ttk.Button(self.frame_content, text='Copy File')
-        copy_file_button.pack()
+        self.copy_file_button = ttk.Button(self.frame_content, text='Copy File')
+        self.copy_file_button.pack()
 
-        delete_file_button = ttk.Button(self.frame_content, text='Delete File')
-        delete_file_button.pack()
+        self.delete_file_button = ttk.Button(self.frame_content, text='Delete File')
+        self.delete_file_button.pack()
 
-        rename_file_button = ttk.Button(self.frame_content, text='Rename File')
-        rename_file_button.pack()
+        self.rename_file_button = ttk.Button(self.frame_content, text='Rename File')
+        self.rename_file_button.pack()
 
-        move_file_button = ttk.Button(self.frame_content, text='Move File')
-        move_file_button.pack()
+        self.move_file_button = ttk.Button(self.frame_content, text='Move File')
+        self.move_file_button.pack()
 
-        make_folder_button = ttk.Button(self.frame_content, text='Make Folder')
-        make_folder_button.pack()
+        self.make_folder_button = ttk.Button(self.frame_content, text='Make Folder')
+        self.make_folder_button.pack()
 
-        copy_folder_button = ttk.Button(self.frame_content, text='Copy Folder')
-        copy_folder_button.pack()
+        self.copy_folder_button = ttk.Button(self.frame_content, text='Copy Folder')
+        self.copy_folder_button.pack()
 
-        delete_folder_button = ttk.Button(self.frame_content, text='Delete Folder')
-        delete_folder_button.pack()
+        self.delete_folder_button = ttk.Button(self.frame_content, text='Delete Folder')
+        self.delete_folder_button.pack()
 
-        list_files_button = ttk.Button(self.frame_content, text='List all Files in Folder')
-        list_files_button.pack()
-
-
+        self.list_files_button = ttk.Button(self.frame_content, text='List all Files in Folder')
+        self.list_files_button.pack()
 
 
 def main():
