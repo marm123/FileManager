e042db359ce35a70f1d2d363c768e2657f2b77c6
Bartek <bartosz.marmolowski@gmail.com>
2022-01-17 12:04:52 +0100

    add required imports and base class

---
+++ FileManager.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import os
+import shutil
+
+
+class FileManager:
+    def __init__(self, master):
+        pass
+
+
+def main():
+    root = Tk()
+    root.title('FileManager')
+    file_manager = FileManager(root)
+    root.mainloop()
+
+
+if __name__ == "__main__": main()
